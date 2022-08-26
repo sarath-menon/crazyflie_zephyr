69b86e3993bd1f65d1acd59fdb6297343da30f6a
sarath-menon <sarathmenon.downey@gmail.com>
2022-08-26 12:00:37 +0530

    use devices struct to group peropherals

---
+++ app/sample_app/include/devices.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/zephyr.h>
+
+struct devices {
+  // console
+  constexpr const static struct device *console =
+      DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+
+  // led's
+  constexpr static struct gpio_dt_spec red_led_left =
+      GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
+};
--- app/sample_app/src/main.cpp
+++ app/sample_app/src/main.cpp
@@ -1,3 +1,5 @@
+#include "devices.hpp"
+#include "led.hpp"
 #include "usb_console.hpp"
 #include <zephyr/zephyr.h>
 
@@ -6,16 +8,14 @@ LOG_MODULE_REGISTER(cf_app);
 BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
              "Console device is not ACM CDC UART device");
 
-// console
-const struct device *console_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-
 int main(void) {
-  UsbConsole console(console_dev);
+  UsbConsole console(devices::console);
+  Led red_led_left(devices::red_led_left);
 
   for (;;) {
 
-    printk("Acc chip id\n");
+    red_led_left.toggle();
 
-    k_sleep(K_MSEC(1500));
+    k_sleep(K_MSEC(500));
   }
 }
