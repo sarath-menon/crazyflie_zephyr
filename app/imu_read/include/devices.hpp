01faf53476cb4df0c7bb24c45f2e70fb1c381550
sarath-menon <sarathmenon.downey@gmail.com>
2022-08-30 19:27:06 +0530

    switched to west coast const

--- app/imu_read/include/devices.hpp
+++ app/imu_read/include/devices.hpp
@@ -5,34 +5,35 @@
 
 struct devices {
   // console
-  constexpr const static struct device *console =
+  struct device constexpr const static *const console =
       DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
   // led's
-  constexpr static struct gpio_dt_spec red_led_left =
+  struct gpio_dt_spec constexpr static red_led_left =
       GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
-  constexpr static struct gpio_dt_spec green_led_left =
+  struct gpio_dt_spec constexpr static green_led_left =
       GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
-  constexpr static struct gpio_dt_spec green_led_right =
+  struct gpio_dt_spec constexpr static green_led_right =
       GPIO_DT_SPEC_GET(DT_ALIAS(led3), gpios);
-  constexpr static struct gpio_dt_spec red_led_right =
+  struct gpio_dt_spec constexpr static red_led_right =
       GPIO_DT_SPEC_GET(DT_ALIAS(led4), gpios);
-  constexpr static struct gpio_dt_spec blue_led_left =
+  struct gpio_dt_spec constexpr static blue_led_left =
       GPIO_DT_SPEC_GET(DT_ALIAS(led5), gpios);
 
   // communication peripherals
-  constexpr const static struct device *i2c = DEVICE_DT_GET(DT_NODELABEL(i2c3));
+  struct device constexpr const static *const i2c =
+      DEVICE_DT_GET(DT_NODELABEL(i2c3));
   // const static struct device *uart6 =
   // DEVICE_DT_GET(DT_NODELABEL(usart6)); const static struct device *spi =
   // DEVICE_DT_GET(DT_NODELABEL(spi1));
 
   // motors
-  constexpr const static struct pwm_dt_spec motor1 =
+  struct pwm_dt_spec constexpr const static motor1 =
       PWM_DT_SPEC_GET(DT_ALIAS(motor1));
-  constexpr const static struct pwm_dt_spec motor2 =
+  struct pwm_dt_spec constexpr const static motor2 =
       PWM_DT_SPEC_GET(DT_ALIAS(motor2));
-  constexpr const static struct pwm_dt_spec motor3 =
+  struct pwm_dt_spec constexpr const static motor3 =
       PWM_DT_SPEC_GET(DT_ALIAS(motor3));
-  constexpr const static struct pwm_dt_spec motor4 =
+  struct pwm_dt_spec constexpr const static motor4 =
       PWM_DT_SPEC_GET(DT_ALIAS(motor4));
 };
